153139bf97f9db9765347cef33693e196c1795f8
Sagar1917118 <112215153@cse.iiitp.ac.in>
2025-03-08 18:21:24 +0530

    adjusting cors paramaters

--- Backend/index.js
+++ Backend/index.js
@@ -17,6 +17,8 @@ const fileUpload=require("express-fileupload");
 app.use(cors({
     origin:[process.env.FRONTEND_URL],
     credentials:true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }))
 
 app.use(express.json());
