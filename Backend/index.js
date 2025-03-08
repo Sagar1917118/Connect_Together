bc11714f0d539a2ff151c6e9ae831cbe756e6d99
Sagar1917118 <112215153@cse.iiitp.ac.in>
2025-03-08 19:53:14 +0530

    cors error 9

--- Backend/index.js
+++ Backend/index.js
@@ -17,17 +17,12 @@ const fileUpload=require("express-fileupload");
 
 const allowedOrigins = [
   "http://localhost:3000",
-   process.env.FRONTEND_URL
+   process.env.FRONTEND_URL,
+   `${process.env.FRONTEND_URL}/`
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, origin);
-      } else {
-          callback(new Error("Not allowed by CORS"));
-      }
-  },
+  origin: allowedOrigins,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
