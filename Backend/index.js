3fce0357f7dae19a655489e831776133395275ee
Sagar1917118 <112215153@cse.iiitp.ac.in>
2025-03-08 18:25:13 +0530

    chekcking cors issue

--- Backend/index.js
+++ Backend/index.js
@@ -30,6 +30,19 @@ app.use(fileUpload(
     ));
 dbConnect();
 // cloudinaryConnect();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+  }
+
+  next();
+});
 app.use("/api/v1/auth",authRoute);
 app.use("/api/v1/profile",profileRoute);
 app.use("/api/v1/course",courseRoute);
